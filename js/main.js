4ed1a171619b6bd934bac37601f37d6ad1a4ea8b
zena <e.nosazena@gmail.com>
2014-03-23 15:17:31 -0400

    added media query and js for mobile nav menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+ ///////modile menu ////////
+
+ $('a.menu-icon').click(function() {
+ 	$('#menu-links').slideToggle();
+ })
+
+ //fix hidden links on window resize
+
+ $(window).resize(function() {
+ 	if ($(window).width() > 700) {
+ 		$('#menu-links').removeAttr('style');
+ 	}
+ })
+
+});
